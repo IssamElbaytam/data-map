db0cb3201eba3048b03f591dcc7dd9be6f92e5ae
sethvincent <sethvincent@gmail.com>
2015-11-07 21:23:28 -0800

    refactor

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
-var BaseElement = require('base-element')
-var inherits = require('inherits')
-var L = require('leaflet')
-require('mapbox.js')
+var emitter = require('component-emitter')
+var leafletMap = require('virtual-leaflet')
 
-module.exports = DataMap
-inherits(DataMap, BaseElement)
+module.exports = function createDataMap (options) {
+  options = options || {}
+  var map = {}
+  emitter(map)
 
-function DataMap (options) {
-  if (!(this instanceof DataMap)) return new DataMap(options)
-  BaseElement.call(this, options.el)
-  var self = this
-  options.tiles = options.tiles || 'mapbox.streets'
-  this.addEventListener('load', function (node) {
-    var mapEl = node.childNodes[0]
-    var tiles = options.tiles
-    L.mapbox.accessToken = options.accessToken
-    options.popupOptions = null
-    self.map = L.mapbox.map(mapEl, tiles, options)
-    self.geojson = L.mapbox.featureLayer().addTo(self.map)
-    self.geojson.on('click', function (e) {
-      self.send('click', e, e.layer.feature, e.target)
-    })
-  })
-}
+  options.onclick = function (e) {
+    map.emit('click', e)
+  }
 
-DataMap.prototype.setGeoJSON = function (geojson) {
-  this.geojson.setGeoJSON(geojson)
-}
-
-DataMap.prototype.render = function (state) {
-  if (this.geojson && state.geojson) this.setGeoJSON(state.geojson)
-  var vtree = this.html('div#map-container', this, [
-    this.html('div#map')
-  ])
-  return this.afterRender(vtree)
+  map.render = function dataMap_render (state) {
+    return this.html('div#map-container', [
+      this.html('div#map', leafletMap(state.geojson, options))
+    ])
+  }
 }
